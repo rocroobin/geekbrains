ab16c13bb44ceb3715b969b7ca1448f7494e7b7e
rocroobin <sizpw@mail.ru>
2018-06-08 23:30:49 +0300

    чуть переделал первое задание.

--- task1.js
+++ task1.js
@@ -7,13 +7,9 @@ function sumPowerOfE(base, exp) {
   return result;
 }
 
-function вывод(b) {
-  console.log(b);
-}
-
 const paramN = process.argv[2];
 const paramE = process.argv[3];
 const n = parseInt(paramN, 10);
 const e = parseInt(paramE, 10);
 const sum = sumPowerOfE(n, e);
-вывод(sum);
+console.log(sum);
