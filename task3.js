a7bb8b445146b3cd0ae9e1d0c889bf2476427e93
rocroobin <sizpw@mail.ru>
2018-06-12 19:59:50 +0300

    сменил названия функций. сделал их более читаемыми и понятными

--- task3.js
+++ task3.js
@@ -1,18 +1,19 @@
 const SECONDS_IN_HOUR = 3600;
 const MINUTES_IN_HOUR = 60;
-function hour(a){
+
+function getHoursCount(a){
     const h1 = a / SECONDS_IN_HOUR;
     const h2 = Math.floor(h1);
     return h2;
 }
 
-function minute(b){
+function getMinutesCount(b){
     const m1 = (b - Math.floor(b/SECONDS_IN_HOUR) * SECONDS_IN_HOUR) / MINUTES_IN_HOUR;
     const m2 = Math.floor(m1);      
     return m2;
 }
 
-function sekond(c){
+function getSecondsCount(c){
     const s1 = c - Math.floor(c/SECONDS_IN_HOUR) * SECONDS_IN_HOUR - Math.floor((c - Math.floor(c/SECONDS_IN_HOUR) * SECONDS_IN_HOUR) / MINUTES_IN_HOUR) * MINUTES_IN_HOUR;
     return s1;
 }
@@ -27,15 +28,15 @@ function plural(count, form1, form2, form3) {
     return form3;
 }
 
-function hours(k) {
+function getHoursText(k) {
     return plural(k, "час", "часа", "часов");
 }
 
-function minutes(l) {
+function getMinutesText(l) {
     return plural(l, "минута", "минуты", "минут");
 }
 
-function sekonds(n) {
+function getSecondsText(n) {
     return plural(n, "секунда", "секунды", "секунд");
 }
 
@@ -44,11 +45,11 @@ const t1 = parseInt(paramT1, 10);
 const paramT2 = process.argv[3];
 const t2 = parseInt(paramT2, 10);
 const sum = t1 + t2;
-const h = hour(sum);
-const hr = hours(h);
-const m = minute(sum);
-const mi = minutes(m);
-const s = sekond(sum);
-const se = sekonds(s);
+const h = getHoursCount(sum);
+const hr = getHoursText(h);
+const m = getMinutesCount(sum);
+const mi = getMinutesText(m);
+const s = getSecondsCount(sum);
+const se = getSecondsText(s);
 const result = [h, hr, m, mi, s, se].join(' ');
 console.log(result);
